782ebaa41269f0648770d007d86eba3e144bf8e5
Aksavvidi <aksavvidi@gmail.com>
2023-04-18 17:06:28 +0300

    swagger  for products

--- swagger.js
+++ swagger.js
@@ -192,10 +192,26 @@ exports.options = {
                 }
             }
         },
+        "/api/userproduct/findAll":{
+            "get":{
+                "tags": [
+                    "User and Products"
+                ],
+                "summary": "Get all Users with their products  in system",
+                "responses":{
+                    "200":{
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/User and Products"
+                        }
+                    }
+                }
+            }
+        },
         "/api/userproduct/findOne/{username}": {
             "get":{
                 "tags": [
-                    "User nad Products"
+                    "User and Products"
                 ],
                 "parameters":[{
                     "name": "username",
@@ -210,6 +226,214 @@ exports.options = {
                 }
 
             }
+        },
+        "/api/userproduct/create":{
+            "post": {
+                "tag":[
+                    "User and Products"
+                ],
+                "description": "Create new product for user in system",
+                "parameters":[{
+                    "name": "Parameters for User and Products",
+                    "in" : "body",
+                    "description": "Product that we want to add to user",
+                    "schema":{
+                       "type":"object",
+                       "properties":{
+                        "username": {"type":"string"},
+                        "product": { "type": "string" },
+                        "cost": { "type": "number" },
+                        "quantity": { "type": "number"},
+                    },
+                    "required": ["username", "product"]
+                  }    
+                }],
+                "produces":["application/json"],
+                "responses":{
+                    "200":{
+                        "description": "New product is created",
+                        "schema":{
+                            "$ref":"#/definitions/Products"
+                        }
+                    }
+                }
+            }
+        },
+        "/api/userproduct/update":{
+            "patch":{
+                "tags":[
+                    "User and Products"
+                ],
+                "description":"Update product from user system",
+                "parameters":[{
+                    "name": "update product from user in system",
+                    "in":"body",
+                    "description": "Product of user that we will update",
+                    "schema":{
+                        "type": "object",
+                        "properties":{
+                            "username": {"type":"string"},
+                            "product": { "type": "string" },
+                            "cost": { "type": "number" },
+                            "quantity": { "type": "number"},
+                        },
+                        "required":["username", "product"]
+                    }
+                }],
+                "produces":["application/json"],
+                "responses": {
+                    "200":{
+                        "description": "Update a product of user",
+                   
+                    }
+            }
+        },
+        "/api/userproduct/delete/{username}/{id}":{
+            "delete":{
+                "tags":[
+                    "User and Products"
+                ],
+                "description":"Delete product from user in system",
+                "parameters":[{
+                    "name":"username",
+                    "in": "path",
+                    "description": "Username that we want to find"
+                },
+                    {
+                    "name":"product",
+                    "in": "path",
+                    "description": "Product that we will delete"
+                }],
+                "response":{
+                    "200":{
+                        "description": "Delete a product from user"
+                    }
+                }
+            }
+        },
+        "/api/products/findAll":{
+            "get":{
+                "tags": [
+                    "Products"
+                ],
+                "summary": "Get all Products  in system",
+                "responses":{
+                    "200":{
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/Products"
+                        }
+                    }
+                }
+            }
+        },
+        "/api/products/findOne/{id}":{
+            "get":{
+                "tags": [
+                    "Products"
+                ],
+                "parameters":[
+                    {
+                        "name": "id",
+                        "in": "path",
+                        "required": true,
+                        "description": "Id of product",
+                        "type":"string"
+                    }
+                ],
+                "summary": "Get a product in system",
+                "responses":{
+                    "200":{
+                        "description": "Product find",
+                        "schema": {
+                            "$ref": "#/definitions/Products"
+                        }
+                    }
+                }
+            }
+        },
+        "/api/products/create":{
+            "post": {
+                "tag":[
+                    "Products"
+                ],
+                "description": "Create new product in app",
+                "parameters":[{
+                    "name": "Parameters for products",
+                    "in" : "body",
+                    "description": "Product parameters that we will create",
+                    "schema":{
+                       // "$ref": "#/definitions/Products"
+                       "type":"object",
+                       "properties":{
+                        "product": { "type": "string" },
+                        "cost": { "type": "number" },
+                        "description": { "type": "string" },
+                        "quantity": { "type": "number"},
+                    },
+                    "required": ["product", "id"]
+                  }    
+                }],
+                "produces":["application/json"],
+                "responses":{
+                    "200":{
+                        "description": "New product is created",
+                        "schema":{
+                            "$ref":"#/definitions/Products"
+                        }
+                    }
+                }
+            }
+        },
+        "/api/products/update":{
+            "patch":{
+                "tags":[
+                    "Products"
+                ],
+                "description":"Update product in system",
+                "parameters":[{
+                    "name": "update product in system",
+                    "in":"body",
+                    "description": "Product that we will update",
+                    "schema":{
+                        "type": "object",
+                        "properties":{
+                            "product": { "type": "string" },
+                            "cost": { "type": "number" },
+                            "description": { "type": "string" },
+                            "quantity": { "type": "number"},
+                        },
+                        "required":["product"]
+                    }
+                }],
+                "produces":["application/json"],
+                "responses": {
+                    "200":{
+                        "description": "Update product",
+                   
+                    }
+            }
+        },
+        "/api/products/delete/{product}":{
+            "delete":{
+                "tags":[
+                    "Products"
+                ],
+                "description":"Delete product from the system",
+                "parameters":[{
+                    "name":"product",
+                    "in": "path",
+                    "description": "Product that we will delete"
+                }],
+                "response":{
+                    "200":{
+                        "description": "Deleted product"
+                    }
+                }
+            }
+        },
+
         }
     }
+}
 }
